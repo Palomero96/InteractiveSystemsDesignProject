65a1b8360a47ff043148197954903f874abd0468
CarlosUni <100074907@alumnos.uc3m.es>
2019-04-16 13:55:28 +0200

    calendario funciona, no se ven eventos aun, no hay

--- PadelClub/src/pages/tab1/tab1.page.ts
+++ PadelClub/src/pages/tab1/tab1.page.ts
@@ -11,7 +11,7 @@ import { NgCalendarModule  } from 'ionic2-calendar';
 })
 export class Tab1Page {
   eventSource = [];
-  viewTitle = 'Calendario';
+  viewTitle = 'Mi Calendario';
   selectedDay = new Date();
   calendar = {
     mode: 'month',
@@ -19,5 +19,22 @@ export class Tab1Page {
   };
 
   constructor(public navCtrl: NavController, private modalCtrl: ModalController, private alertCtrl: AlertController) { }
+
+  onViewTitleChanged(title) {
+    this.viewTitle = title;
+  }
+  // onEventSelected(event) {
+  //   let start = moment(event.startTime).format('LLLL');
+  //   let end = moment(event.endTime).format('LLLL');
+  //   let alert = this.alertCtrl.create({
+  //     title: '' + event.title,
+  //     subTitle: 'From: ' + start + '<br>To: ' + end,
+  //     buttons: ['OK']
+  //   })
+  //   alert.present();
+  // }
+  onTimeSelected(ev) {
+    this.selectedDay = ev.selectedTime;
+  }
 
 }
