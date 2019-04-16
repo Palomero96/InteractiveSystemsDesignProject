4da7ccf3cb44271753c2360d6002750132dcd357
CarlosUni <100074907@alumnos.uc3m.es>
2019-04-16 16:31:24 +0200

    integrado master con calendar

--- PadelClub/src/models/mensaje.model.ts
+++ PadelClub/src/models/mensaje.model.ts
@@ -1,16 +1,6 @@
-<<<<<<< HEAD
-export interface Mensaje {
-    id: string;
-    origen: string;
-    destinatario: string;
-    contenido: string;
-    //tiempo: Date;
-}
-=======
 export interface Mensaje {
     id: string;
     origen: string;
     destinatario: string;
     contenido: string;
 }
->>>>>>> calendar_carlos
