17fd67f75bfe8602736a9075fca7b89cccf642e2
jortesal <jortesal@gmail.com>
2019-04-18 15:49:10 +0200

    Añade amigos
    Añade amigos

--- PadelClub/src/pages/add-contacto/add-contacto.ts
+++ PadelClub/src/pages/add-contacto/add-contacto.ts
@@ -3,6 +3,9 @@ import { Contact } from './../../models/contact.model';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { Observable } from 'rxjs/Observable';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { TabsPage } from '../tabs/tabs';
 
 /**
  * Generated class for the AddContactoPage page.
@@ -20,7 +23,8 @@ export class AddContactoPage {
   contacts$: Observable<Contact[]>;
 
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private ContactService: ContactService) {
+  constructor(private afAuth: AngularFireAuth, private afDataBase: AngularFireDatabase,
+    public navCtrl: NavController, public navParams: NavParams, private ContactService: ContactService) {
   }
 
   ionViewWillEnter(){
@@ -30,6 +34,13 @@ export class AddContactoPage {
    
   ionViewDidLoad() {
     console.log('ionViewDidLoad AddContactoPage');
+  }
+
+  addAmigo(value)
+  {
+    this.afAuth.authState.take(1).subscribe(auth => {
+      this.afDataBase.object(`perfil/${auth.uid}/amigos/`).set({id:value}).then(() => this.navCtrl.pop());
+    })
   }
 
   back()
