c1c977b11583181069aa7bb4b6184fbaa6f624cf
Palomero96 <31955373+Palomero96@users.noreply.github.com>
2019-04-20 19:17:32 +0200

    Clases
    Se muestran la informacion de las clases parcialmente ( faltaria añadir la informacion acerca de los alumnos que pertenecen a dicha clase y demas)

--- PadelClub/src/pages/tab3/tab3.ts
+++ PadelClub/src/pages/tab3/tab3.ts
@@ -6,6 +6,7 @@ import { AngularFireDatabase, AngularFireObject } from 'angularfire2/database';
 import { dateSortValue } from 'ionic-angular/umd/util/datetime-util';
 import { AddPerfilPage } from '../add-perfil/add-perfil';
 import { Contact } from '../../models/contact.model';
+import { Clase } from '../../models/clase.model';
 
 /**
  * Generated class for the Tab3Page page.
@@ -25,26 +26,41 @@ export class Tab3Page {
   myUserData: {};
   datosPerfil: AngularFireObject<Contact>;;
   Perfil:Contact;
+  rol:string;
+  clases:Observable<Clase[]>;
 
   constructor(public navCtrl: NavController,private afAuth: AngularFireAuth, private afDataBase: AngularFireDatabase, private crearClaseMod: ModalController, public navParams: NavParams) {
-    this.esProfesor = false;   
+    this.esProfesor = false;
   }
 
-  ionViewDidLoad() {
+  ionViewWillEnter() {
 
-    this.afAuth.authState.take(1).subscribe(async data=>{
+    this.afAuth.authState.take(1).subscribe( async data=>{
       //De esta manera el id sera el mismo da igual quien cree la conversacion
-            
         this.datosPerfil = this.afDataBase.object(`perfil/${data.uid}`);
-        this.datosPerfil.snapshotChanges().subscribe(async action => {
+        console.log(this.datosPerfil)
+        this.datosPerfil.snapshotChanges().subscribe( async action => {
         this.Perfil = await action.payload.val();
         //asignamos la variable esProfesor para mostrar una cosa u otra en el html
         if(this.Perfil.rol=="Profesor"){
           this.esProfesor=true;
-        }  
-
+          this.clases=this.afDataBase.list<Clase>(`clase/${data.uid}`).valueChanges();
+          console.log(this.clases);
+        } else{
+          //Aqui tendremos que obtener las clases en las que se puede apuntar el alumno
+        }
+        
+    
+      /*await this.esProfesor;
+      console.log(" DEspues del await " + this.esProfesor)
+      if(this.esProfesor==true){
+        this.clases=this.afDataBase.list(`clase/${data.uid}`).valueChanges();
+          console.log(this.clases);
+      }
+      console.log(this.esProfesor)*/
+        });
       });
-      });
+      
   }
 
   crearClase() {
