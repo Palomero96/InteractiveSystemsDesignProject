cf807e6fea9f5acc2c27b9b0436e4391629e0179
Palomero96 <31955373+Palomero96@users.noreply.github.com>
2019-04-20 13:13:24 +0200

    Pequeño cambio
    sad
    
    Co-Authored-By: jortesal <jortesal@users.noreply.github.com>

--- PadelClub/src/pages/tab3/tab3.ts
+++ PadelClub/src/pages/tab3/tab3.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 import { Modal, ModalOptions, IonicPage, NavController, NavParams, ModalController } from 'ionic-angular';
+import { Observable } from 'rxjs/Observable';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFireDatabase, AngularFireObject } from 'angularfire2/database';
+import { dateSortValue } from 'ionic-angular/umd/util/datetime-util';
+import { AddPerfilPage } from '../add-perfil/add-perfil';
+import { Contact } from '../../models/contact.model';
 
 /**
  * Generated class for the Tab3Page page.
@@ -17,13 +23,28 @@ export class Tab3Page {
 
   esProfesor: boolean;
   myUserData: {};
+  datosPerfil: AngularFireObject<Contact>;;
+  Perfil:Contact;
 
-  constructor(public navCtrl: NavController, private crearClaseMod: ModalController, public navParams: NavParams) {
-    this.esProfesor = true;   // TODO: Esto deberá variar, fijado a true para tests
+  constructor(public navCtrl: NavController,private afAuth: AngularFireAuth, private afDataBase: AngularFireDatabase, private crearClaseMod: ModalController, public navParams: NavParams) {
+    this.esProfesor = false;   
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad Tab3Page');
+
+    this.afAuth.authState.take(1).subscribe(async data=>{
+      //De esta manera el id sera el mismo da igual quien cree la conversacion
+            
+        this.datosPerfil = this.afDataBase.object(`perfil/${data.uid}`);
+        this.datosPerfil.snapshotChanges().subscribe(async action => {
+        this.Perfil = await action.payload.val();
+        //asignamos la variable esProfesor para mostrar una cosa u otra en el html
+        if(this.Perfil.rol=="Profesor"){
+          this.esProfesor=true;
+        }  
+
+      });
+      });
   }
 
   crearClase() {
